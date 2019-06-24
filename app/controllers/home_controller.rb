d915fabc9bacd2642a30f42d0d396dcf7d18df81
Ciarra Peters <ciarra.peters@appfolio.com>
2019-06-24 15:55:45 -0700

    cnp - made tags clickable and filterable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @images = Image.all.order(created_at: :desc)
+    @images = if params[:tag]
+                Image.tagged_with(params[:tag]).order(created_at: :desc)
+              else
+                Image.all.order(created_at: :desc)
+              end
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,8 +4,10 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   setup do
     image = Image.create!(url: 'https://media.glassdoor.com/employerbenefits/o/1/appfolio-employerbenefits-1429212670338.jpg',
                           tag_list: 'appfolio, office')
-    image2 = Image.create!(url: 'https://media.glassdoor.com/employerbenefits/o/2/appfolio-employerbenefits-1429212670338.jpg')
-    image3 = Image.create!(url: 'https://media.glassdoor.com/employerbenefits/o/3/appfolio-employerbenefits-1429212670338.jpg')
+    image2 = Image.create!(url: 'https://media.glassdoor.com/employerbenefits/o/2/appfolio-employerbenefits-1429212670338.jpg',
+                           tag_list: 'california, best workplaces')
+    image3 = Image.create!(url: 'https://media.glassdoor.com/employerbenefits/o/3/appfolio-employerbenefits-1429212670338.jpg',
+                           tag_list: 'appfolio, office, best workplaces')
     @images = [image, image2, image3]
   end
 
@@ -21,5 +23,26 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
       end
     end
     assert_select '#tags', "Tags: #{@images.first.tag_list}"
+  end
+
+  test 'filtering based on tags' do
+    get tag_url(@images[0].tag_list.first)
+
+    assert_response :success
+    assert_select '#tags', 'Tags: appfolio, office, best workplaces'
+    assert_select 'img' do
+      assert_select '[src=?]', @images[0].url
+      assert_select '[src=?]', @images[2].url
+      assert_select '[src=?]', @images[1].url, false
+    end
+  end
+
+  test 'clickability of tags' do
+    get tag_url(@images[0].tag_list.first)
+
+    assert_select 'a[href=?]', '/tags/appfolio'
+    assert_select 'a[href=?]', '/tags/office'
+    assert_select 'a[href=?]', '/tags/california', count: 0
+    assert_select 'a[href=?]', '/tags/best%20workplaces'
   end
 end
