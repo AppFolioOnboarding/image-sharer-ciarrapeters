c3f5ae9ebce094136002551ea13e0209c177bd6b
Ciarra Peters <ciarra.peters@appfolio.com>
2019-06-24 17:23:55 -0700

    cnp - Add image model validations

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'image valid' do
+    image = Image.new(url: 'https://media.glassdoor.com/employerbenefits/o/225531/appfolio-employerbenefits-1429212670338.jpg')
+
+    assert_predicate image, :valid?
+  end
+
+  test 'image invalid if blank' do
+    image = Image.new(url: '')
+
+    assert_not_predicate image, :valid?
+    assert_equal "can't be blank", image.errors.messages[:url].first
+  end
+
+  test 'image invalid if not proper image extension' do
+    image = Image.new(url: 'https://media.glassdoor.com/employerbenefits/o')
+
+    assert_not_predicate image, :valid?
+    assert_equal 'is invalid', image.errors.messages[:url].first
+  end
+
+  test 'image invalid if not proper uri' do
+    image = Image.new(url: 'asdklfjaskldf')
+
+    assert_not_predicate image, :valid?
+    assert_equal 'must be a valid url', image.errors.messages[:url].first
+  end
 end
