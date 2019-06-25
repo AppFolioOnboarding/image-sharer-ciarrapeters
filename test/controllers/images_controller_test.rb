58cbfad0b28479d4844c0aebc030ef1312d3b6c7
Ciarra Peters <ciarra.peters@appfolio.com>
2019-06-24 17:32:53 -0700

    cnp - Add ability to show image

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @image = Image.create!(url: 'https://learn.appfolio.com/apm/www/images/apm-logo-v2.png')
+  end
+
   test 'new image path appears correctly' do
     get new_image_path
 
@@ -8,5 +12,15 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_select '#header', 'New Image'
     assert_select 'form[action=?]', '/images'
     assert_select 'input[name=?]', 'image[url]'
+  end
+
+  test 'show image path appears correctly' do
+    get image_path(@image.id)
+
+    assert_response :ok
+    assert_select '#header', 'Your beautiful image:'
+    assert_select 'img' do
+      assert_select '[src=?]', @image.url
+    end
   end
 end
