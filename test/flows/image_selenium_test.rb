61bf0c306c5bf086ec7abce2f99b0872f2d4b8ad
Ciarra Peters <ciarra.peters@appfolio.com>
2019-07-01 11:30:35 -0700

    cnp - Add selenium test for deleting images

--- test/flows/image_selenium_test.rb
+++ test/flows/image_selenium_test.rb
@@ -1,4 +1,4 @@
-require '/Users/ciarra.peters/image-sharer-ciarrapeters/test/test_helper.rb'
+require 'test_helper.rb'
 
 class ImageTestSelenium < ActionDispatch::IntegrationTest
   def test_add_image
@@ -14,4 +14,27 @@ class ImageTestSelenium < ActionDispatch::IntegrationTest
     assert_equal 'Tags: appfolio', image_show_page.image_tags
   end
 
+  def test_delete_image
+    image_url = 'https://learn.appfolio.com/apm/www/images/apm-logo-v2.png'
+    image_tags = 'appfolio'
+    image_tags2 = 'appfolio, santa barbara'
+
+    image_params = { url: image_url, tag_list: image_tags }
+    image = Image.new(image_params)
+    image.save!
+
+    image_params2 = { url: image_url, tag_list: image_tags2 }
+    image2 = Image.new(image_params2)
+    image2.save!
+
+    index_page = Images::IndexPage.visit
+    assert_equal 2, index_page.images.count
+
+    index_page2 = index_page.images.first.delete_image! do |alert|
+      assert_equal 'Are you sure?',
+                   alert.text
+    end
+
+    assert_equal 1, index_page2.images.size
+  end
 end
--- test/page_objects/images/index_page.rb
+++ test/page_objects/images/index_page.rb
@@ -7,6 +7,19 @@ module Images
     collection :images, locator: 'tbody', item_locator: 'tr' do
       element :image, locator: '.js-image-url'
       element :tags, locator: '.js-image-tags'
+      form_for :delete_button, locator: '.js-delete-button' do
+        element :button, locator: '.js-delete-image-link'
+      end
+
+      def delete_image!
+        delete_button.button.node.click
+        alert = node.base.driver.browser.switch_to.alert
+        yield alert if block_given?
+        alert.accept
+        sleep 3
+        stale!
+        window.change_to(IndexPage)
+      end
     end
 
     def add_image!
@@ -14,6 +27,5 @@ module Images
       stale!
       window.change_to(NewPage)
     end
-
   end
 end
