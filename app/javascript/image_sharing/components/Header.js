7f4f03e6a57a5b7d10b6c992dc3209132f3f834e
Ciarra Peters <ciarra.peters@appfolio.com>
2019-07-01 12:06:09 -0700

    cnp - Test CI

--- app/javascript/image_sharing/components/Header.js
+++ app/javascript/image_sharing/components/Header.js
@@ -16,6 +16,7 @@ class Header extends Component {
             <h3 className='text-center'>
               {title}
             </h3>
+            <h3>Test</h3>
           </Col>
         </Row>
       </div>
