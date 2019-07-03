d22e8fcaf02303d40f31836eb829c24f0eae44ac
Ciarra Peters <ciarra.peters@appfolio.com>
2019-07-03 10:22:34 -0700

    cnp - Edit Footer component and add tests

--- app/javascript/image_sharing/components/Footer.js
+++ app/javascript/image_sharing/components/Footer.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 class Footer extends Component {
-  /* Implement your Footer component here */
+  static propTypes = {
+    subtext: PropTypes.string.isRequired
+  };
+
+  render() {
+    const subtext = this.props.subtext;
+    return (
+      <div>
+        <Row>
+          <Col>
+            <p className='text-center' style={{fontSize: '10px'}}>
+              {subtext}
+            </p>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Footer;
