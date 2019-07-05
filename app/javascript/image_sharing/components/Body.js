1c45ddeeffde840678c31e50831005c0b64f7801
Ciarra Peters <ciarra.peters@appfolio.com>
2019-07-05 11:56:00 -0700

    cnp - Add Body and FeedbackStore to App

--- app/javascript/image_sharing/components/Body.js
+++ app/javascript/image_sharing/components/Body.js
@@ -3,12 +3,22 @@ import { Row, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import FeedbackForm from './FeedbackForm';
+import { PostFeedbackService } from '../services/PostFeedbackService';
 
 @observer
 class Body extends Component {
   static propTypes = {
     store: PropTypes.object.isRequired
   };
+
+  onClickHandler = () => {
+    const params = {
+      name: this.props.store.userName,
+      comments: this.props.store.comments
+    };
+    const service = new PostFeedbackService(this.props.store, params);
+    return service.postFeedback();
+  }
 
   render() {
     return (
